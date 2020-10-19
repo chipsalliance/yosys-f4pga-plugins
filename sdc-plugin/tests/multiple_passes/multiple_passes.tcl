245a79321d046eef978cabf4a88cc9e651959f76
Tomasz Michalak <tmichalak@antmicro.com>
2020-10-19 13:21:24 +0200

    SDC: Add test for multiple pass write_sdc
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

---
+++ sdc-plugin/tests/multiple_passes/multiple_passes.tcl
@@ -0,0 +1,17 @@
+yosys -import
+
+plugin -i sdc
+
+yosys -import
+
+read_verilog $::env(DESIGN_TOP).v
+synth_xilinx
+create_clock -period 10 clk
+write_sdc $::env(DESIGN_TOP)_1.sdc
+write_json $::env(DESIGN_TOP).json
+
+design -push
+
+read_json $::env(DESIGN_TOP).json
+create_clock -period 10 clk
+write_sdc $::env(DESIGN_TOP)_2.sdc
