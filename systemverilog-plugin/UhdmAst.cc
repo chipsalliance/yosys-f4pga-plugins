dd0d6c9c404c69535f56d3d89102e35db97e6a1e
Rafal Kapuscik <rkapuscik@antmicro.com>
2022-11-10 14:03:33 +0100

    Fix file location defines
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- systemverilog-plugin/UhdmAst.cc
+++ systemverilog-plugin/UhdmAst.cc
@@ -1277,16 +1277,16 @@ AST::AstNode *UhdmAst::make_ast_node(AST::AstNodeType type, std::vector<AST::Ast
     if (auto filename = vpi_get_str(vpiFile, obj_h)) {
         node->filename = filename;
     }
-    if (unsigned int first_line = vpi_get(vpiStartLine, obj_h)) {
+    if (unsigned int first_line = vpi_get(vpiLineNo, obj_h)) {
         node->location.first_line = first_line;
     }
-    if (unsigned int last_line = vpi_get(vpiEndLine, obj_h)) {
+    if (unsigned int last_line = vpi_get(vpiEndLineNo, obj_h)) {
         node->location.last_line = last_line;
     }
-    if (unsigned int first_col = vpi_get(vpiColumn, obj_h)) {
+    if (unsigned int first_col = vpi_get(vpiColumnNo, obj_h)) {
         node->location.first_column = first_col;
     }
-    if (unsigned int last_col = vpi_get(vpiEndColumn, obj_h)) {
+    if (unsigned int last_col = vpi_get(vpiEndColumnNo, obj_h)) {
         node->location.last_column = last_col;
     }
     node->children = children;
