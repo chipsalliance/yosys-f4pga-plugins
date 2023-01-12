4c733893c5066496e5775685291ce29f01564c2d
Antoni Pokusinski <apokusinski@internships.antmicro.com>
2023-01-12 13:28:17 +0100

    Simplify xdc sdc builds
    Passing `design_introspection.so` as an artifact to xdc and sdc jobs
    makes the CI workflow overly complicated and error-prone. We can instead
    build this plugin in these jobs too as it takes very little time.

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -28,6 +28,11 @@ fi
 start_section Building
 
 export CXXFLAGS=-Werror
+if [ "$PLUGIN_NAME" == "xdc" ] || [ "$PLUGIN_NAME" == "sdc" ]; then 
+    make design_introspection.so -j`nproc`
+	make install_design_introspection -j`nproc`
+fi 
+
 make UHDM_INSTALL_DIR=`pwd`/env/conda/envs/yosys-plugins/ ${PLUGIN_NAME}.so -j`nproc`
 unset CXXFLAGS
 
